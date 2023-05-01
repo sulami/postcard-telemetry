58cbe590d4e251bd18940a4af691d2f596a8e162
Robin Schroer <git@sulami.xyz>
2023-05-01 20:12:22 +0900

    Rework the logging API
    This is neater now, because one does not have to construct the
    parameter map, and instead can chain .with_field calls.

--- src/log.rs
+++ src/log.rs
@@ -17,16 +17,17 @@ pub struct LogMessage {
 
 impl LogMessage {
     /// Create a new log message.
-    pub fn new(
-        level: Level,
-        message: &'static str,
-        parameters: LinearMap<&'static str, LogParameter, 8>,
-    ) -> Self {
+    pub fn new(level: Level, message: &'static str) -> Self {
         Self {
             level,
             message,
-            parameters,
+            parameters: LinearMap::new(),
         }
+    }
+
+    pub fn with_field(mut self, name: &'static str, parameter: impl Into<LogParameter>) -> Self {
+        let _ = self.parameters.insert(name, parameter.into());
+        self
     }
 }
 
@@ -46,6 +47,9 @@ impl std::fmt::Display for LogMessage {
                 LogParameter::Integer(v) => {
                     message = message.replace(&format!("{{{}}}", name), &format!("{}", v));
                 }
+                LogParameter::UnsignedInteger(v) => {
+                    message = message.replace(&format!("{{{}}}", name), &format!("{}", v));
+                }
             }
         }
         write!(f, "[{}] {}", self.level, message)
@@ -55,6 +59,7 @@ impl std::fmt::Display for LogMessage {
 /// A log message level.
 #[derive(Debug, Clone, Copy, Serialize)]
 pub enum Level {
+    Debug,
     Info,
     Warning,
     Error,
@@ -64,6 +69,7 @@ pub enum Level {
 impl std::fmt::Display for Level {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Level::Debug => write!(f, "DEBUG"),
             Level::Info => write!(f, "INFO"),
             Level::Warning => write!(f, "WARNING"),
             Level::Error => write!(f, "ERROR"),
@@ -77,6 +83,7 @@ pub enum LogParameter {
     String(&'static str),
     Float(f32),
     Integer(i32),
+    UnsignedInteger(u32),
 }
 
 #[cfg(feature = "std")]
@@ -86,7 +93,32 @@ impl std::fmt::Display for LogParameter {
             LogParameter::String(s) => write!(f, "{}", s),
             LogParameter::Float(v) => write!(f, "{}", v),
             LogParameter::Integer(v) => write!(f, "{}", v),
+            LogParameter::UnsignedInteger(v) => write!(f, "{}", v),
         }
+    }
+}
+
+impl From<&'static str> for LogParameter {
+    fn from(s: &'static str) -> Self {
+        Self::String(s)
+    }
+}
+
+impl From<f32> for LogParameter {
+    fn from(v: f32) -> Self {
+        Self::Float(v)
+    }
+}
+
+impl From<i32> for LogParameter {
+    fn from(v: i32) -> Self {
+        Self::Integer(v)
+    }
+}
+
+impl From<u32> for LogParameter {
+    fn from(v: u32) -> Self {
+        Self::UnsignedInteger(v)
     }
 }
 
@@ -100,5 +132,6 @@ mod tests {
         assert_eq!(format!("{}", LogParameter::String("foo")), "foo");
         assert_eq!(format!("{}", LogParameter::Float(1.0)), "1");
         assert_eq!(format!("{}", LogParameter::Integer(1)), "1");
+        assert_eq!(format!("{}", LogParameter::UnsignedInteger(1)), "1");
     }
 }
